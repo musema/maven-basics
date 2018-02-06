3b4f1390c3731454e27ddda5ed66336553e916df
Musema Hassen <musema.hassen@gmail.com>
2018-02-05 22:03:13 -0500

    Add Project service test class

--- src/test/java/com/openlane/demo/serviceTest/ProjectServiceTest.java
+++ src/test/java/com/openlane/demo/serviceTest/ProjectServiceTest.java
@@ -1,6 +1,21 @@
 package com.openlane.demo.serviceTest;
 
-public class ProjectServiceTest {
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
 
+import com.openlane.demo.basic.service.ProjectService;
+
+public class ProjectServiceTest {
+	private ProjectService pService;
+	
+	@Before
+	public void init() {
+		this.pService=new ProjectService();
+	}
+	@Test
+	public void getProjectsByIdTest() {
+		assertTrue(true);
+	}
 
 }
